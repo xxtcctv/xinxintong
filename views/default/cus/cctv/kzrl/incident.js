81ca454f421224efe01d5069b79babfae1899380
yangyue <yangyue@ctsi.com.cn>
2015-06-11 15:27:28 +0800

    增加获取地理位置代码

--- views/default/cus/cctv/kzrl/incident.js
+++ views/default/cus/cctv/kzrl/incident.js
@@ -1,14 +1,62 @@
 var app = angular.module('kzrl', []);
 app.config(['$locationProvider', function ($lp) {
-	$lp.html5Mode(true);
+    $lp.html5Mode(true);
 }]);
 app.controller('ctrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-	var id = $location.search().id, url = '/rest/cus/cctv/kzrl/get';
-	url += '?articleid=' + id;
-	$scope.openNearby = function () {
-		location.href = '/views/default/cus/cctv/kzrl/nearby.html?id=' + id;
-	};
-	$http.get(url).success(function (rsp) {
-		$scope.incident = rsp.data;
-	});
+    var id = $location.search().id, url = '/rest/cus/cctv/kzrl/get';
+    url += '?articleid=' + id;
+    var geoSuccessCallback = function (position) {
+        $scope.position = position;
+        url += '&lat=' + position.coords.latitude;
+        url += '&lng=' + position.coords.longitude;
+        $http.get(url).success(function (rsp) {
+            $scope.incident = rsp.data;
+        });
+    };
+    var geoError = function (error) {
+        var x;
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                x = "User denied the request for Geolocation.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x = "Location information is unavailable.";
+                break;
+            case error.TIMEOUT:
+                x = "The request to get user location timed out.";
+                break;
+            case error.UNKNOWN_ERROR:
+                x = "An unknown error occurred.";
+                break;
+        }
+        console.log('geo error', x);
+        $http.get(url).success(function (rsp) {
+            $scope.incident = rsp.data;
+        });
+    };
+    var requestPosition = function () {
+        var nav;
+        nav = window.navigator;
+        if (nav !== null) {
+            var geoloc = nav.geolocation;
+            if (geoloc !== null) {
+                geoloc.getCurrentPosition(geoSuccessCallback, geoError);
+            }
+            else {
+                alert("Geolocation API is not supported in your browser");
+            }
+        } else {
+            alert("Navigator is not found");
+        }
+    };
+    if (/MicroMessenger/i.test(navigator.userAgent) || /YiXin/i.test(navigator.userAgent)) {
+        requestPosition();
+    } else {
+        $http.get(url).success(function (rsp) {
+            $scope.incident = rsp.data;
+        });
+    }
+    $scope.openNearby = function () {
+        location.href = '/views/default/cus/cctv/kzrl/nearby.html?id=' + id;
+    };
 }]);
--- views/default/cus/cctv/kzrl/today.js
+++ views/default/cus/cctv/kzrl/today.js
@@ -1,14 +1,14 @@
 var app = angular.module('kzrl', []);
 app.controller('ctrl', ['$scope', '$http', function ($scope, $http) {
-	$scope.openOne = function (incident) {
-		location.href = '/views/default/cus/cctv/kzrl/incident.html?id=' + incident.id;
-	};
-	$scope.timeline = function() {
-		$http.get('/rest/cus/cctv/kzrl/timeline').success(function (rsp) {
-			$scope.timelines = rsp.data;
-		});
-	};
-	$http.get('/rest/cus/cctv/kzrl/today').success(function (rsp) {
-		$scope.todays = rsp.data;
-	});
+    $scope.openOne = function (incident) {
+        location.href = '/views/default/cus/cctv/kzrl/incident.html?id=' + incident.id;
+    };
+    $scope.timeline = function () {
+        $http.get('/rest/cus/cctv/kzrl/timeline').success(function (rsp) {
+            $scope.timelines = rsp.data;
+        });
+    };
+    $http.get('/rest/cus/cctv/kzrl/today').success(function (rsp) {
+        $scope.todays = rsp.data;
+    });
 }]);
